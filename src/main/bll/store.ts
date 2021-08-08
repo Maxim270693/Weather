8b50adab8e1ab2b8b759ca9771c704c610036763
Maxim270693 <maxim.zhuk.93@mail.ru>
2021-08-08 12:17:48 +0300

    adding localStorage

--- src/main/bll/store.ts
+++ src/main/bll/store.ts
@@ -1,12 +1,19 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import thunk from "redux-thunk";
 import {weatherReducer} from "./reducers/weatherReducer";
+import {loadState, saveState} from "../../utils/localStorage-utils";
 
 const rootReducer = combineReducers({
     weather: weatherReducer
 })
 
-export const store = createStore(rootReducer, applyMiddleware(thunk))
+export const store = createStore(rootReducer, loadState(), applyMiddleware(thunk))
+
+store.subscribe(() => {
+    saveState({
+        weather: store.getState().weather
+    })
+})
 
 
 export type RootStateType = ReturnType<typeof rootReducer>

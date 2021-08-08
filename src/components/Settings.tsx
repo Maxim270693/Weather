75c2c0785503bdc9561412cecec2b4494432faa5
Maxim270693 <maxim.zhuk.93@mail.ru>
2021-08-08 18:03:52 +0300

    replacement of cities

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ClearIcon from '@material-ui/icons/Clear';
 import MenuIcon from '@material-ui/icons/Menu';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import {useDispatch, useSelector} from "react-redux";
 import {RootStateType} from "../main/bll/store";
-import {CityType, deleteCityAC, setSettingsAC} from "../main/bll/reducers/weatherReducer";
+import {CityType, deleteCityAC, replaceCityAC, setSettingsAC} from "../main/bll/reducers/weatherReducer";
 
 const Settings = () => {
 
     const cities = useSelector<RootStateType, Array<CityType>>(state => state.weather.cities)
     const dispatch = useDispatch()
+
+    const [currentCity, setCurrentCity] = useState<any>(null)
 
     const onClickHandler = () => dispatch(setSettingsAC(false))
 
@@ -17,6 +19,22 @@ const Settings = () => {
         dispatch(deleteCityAC(id))
     }
 
+    const dragStartHandler = (e: React.DragEvent<HTMLDivElement>, city: CityType) => {
+        setCurrentCity(city)
+    }
+
+    const dragEndHandler = (e: React.DragEvent<HTMLDivElement>) => {
+
+    }
+
+    const dragOverHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault()
+    }
+
+    const dropHandler = (e: React.DragEvent<HTMLDivElement>, city: CityType) => {
+        e.preventDefault()
+        dispatch(replaceCityAC(city, currentCity))
+    }
 
     return (
         <div className='wrapper_settings'>
@@ -25,11 +43,21 @@ const Settings = () => {
                 <ClearIcon className='clear_icon' onClick={onClickHandler}/>
             </div>
             {
-                cities.map(city => <div className='menu' key={city.id}>
-                    <MenuIcon/>
-                    <p>{city.title}</p>
-                    <HighlightOffIcon className='delete_icon' onClick={() => removeCity(city.id)}/>
-                </div>)
+
+                cities.map(city =>
+                    <div className='menu'
+                         key={city.id}
+                         draggable={true}
+                         onDragStart={(e) => dragStartHandler(e, city)}
+                         onDragLeave={(e) => dragEndHandler(e)}
+                         onDragEnd={(e) => dragEndHandler(e)}
+                         onDragOver={(e) => dragOverHandler(e)}
+                         onDrop={(e) => dropHandler(e, city)}
+                    >
+                        <MenuIcon/>
+                        <p>{city.title}</p>
+                        <HighlightOffIcon className='delete_icon' onClick={() => removeCity(city.id)}/>
+                    </div>)
             }
         </div>
     );
--- src/main/bll/reducers/weatherReducer.ts
+++ src/main/bll/reducers/weatherReducer.ts
@@ -7,6 +7,7 @@ const SET_ERROR = 'SET_ERROR'
 const SET_CITY = 'SET_CITY'
 const SET_SETTINGS = 'SET_SETTINGS'
 const DELETE_CITY = 'DELETE_CITY'
+const REPLACE_CITY = 'REPLACE_CITY'
 
 export type CityType = {
     id: number
@@ -39,6 +40,18 @@ export const weatherReducer = (state = initialState, action: ActionType): Initia
             return {...state, cities: [...state.cities, action.city]}
         case DELETE_CITY:
             return {...state, cities: state.cities.filter((city) => city.id !== action.id)}
+        case REPLACE_CITY:
+            return {
+                ...state, cities: state.cities.map(c => {
+                    if (c.title === action.city1.title) {
+                        return action.city2
+                    }
+                    if (c.title === action.city2.title) {
+                        return action.city1
+                    }
+                    return c
+                })
+            }
         default:
             return state
     }
@@ -51,6 +64,7 @@ export const setErrorAC = (error: string) => ({type: SET_ERROR, error} as const)
 export const setCityAC = (city: CityType) => ({type: SET_CITY, city} as const)
 export const setSettingsAC = (settings: boolean) => ({type: SET_SETTINGS, settings} as const)
 export const deleteCityAC = (id: number) => ({type: DELETE_CITY, id} as const)
+export const replaceCityAC = (city1: CityType, city2: CityType) => ({type: REPLACE_CITY, city1, city2} as const)
 
 // ThunkCreators
 export const getWeatherTC = (city: string) => async (dispatch: ThunkDispatch<RootStateType, unknown, ActionType>) => {
@@ -71,12 +85,12 @@ export const getWeatherCurrentTC = (lat: number, long: number, api: string) => a
 }
 
 
-
 type GetWeatherType = ReturnType<typeof getWeatherAC>
 type SetErrorType = ReturnType<typeof setErrorAC>
 type SetCityType = ReturnType<typeof setCityAC>
 type SetSettingsType = ReturnType<typeof setSettingsAC>
 type DeleteCityType = ReturnType<typeof deleteCityAC>
+type ReplaceCityType = ReturnType<typeof replaceCityAC>
 
 
-type ActionType = GetWeatherType | SetErrorType | SetCityType | SetSettingsType | DeleteCityType
+type ActionType = GetWeatherType | SetErrorType | SetCityType | SetSettingsType | DeleteCityType | ReplaceCityType
